45a4997d60a87a0f11d6332c8a104cecb04cbfef
Maytepg <115984184+Maytepg@users.noreply.github.com>
2023-05-07 18:42:21 +0200

    Update Git.java
    commiteando desde GitHub

--- src/ControlVersiones/Git.java
+++ src/ControlVersiones/Git.java
@@ -4,6 +4,7 @@ public class Git {
 	public static void main(String[] args) {		
 		System.out.println("Empezando proyecto");
 		System.out.println ("realizando la prueba de Git”");
+		System.out.println ("línea añadida desde GitHub");
 	}
 
 }
