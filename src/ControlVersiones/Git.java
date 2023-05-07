c9a56f07d728312e8fafe40a755b2a6acfa07c70
Mayte <mtpedrazg@educa.jcyl.es>
2023-05-07 18:33:30 +0200

    mi segundo commit desde Eclipse

--- src/ControlVersiones/Git.java
+++ src/ControlVersiones/Git.java
@@ -3,6 +3,7 @@ package ControlVersiones;
 public class Git {
 	public static void main(String[] args) {		
 		System.out.println("Empezando proyecto");
+		System.out.println ("realizando la prueba de Git”");
 	}
 
 }
